4994bfd4ad02d8e87f7bda82bbbbb4d87bf51c6a
shi guang <shiguang124@outlook.com>
2021-10-30 16:13:33 +0800

    write my test page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+
+    line = '''
+    <head>
+            <meta charset="utf-8">
+            <title>测试网站标题</title>
+    </head>
+    <h2 style="text-align: center" >测试网页</h2>
+    <hr>
+    <h3 style="text-align: center" ><a href="/play/">进入游戏<a></h3>
+    <img style="width: 98vw" src="https://s3.bmp.ovh/imgs/2021/10/66d3480b484ae815.jpeg">'''
+    return HttpResponse(line)
+
+
+def play(request):
+    line = '''
+     <head>
+            <meta charset="utf-8">
+            <title>术士之战</title>
+    </head>
+    <h2 style="text-align: center" >游戏界面</h2>
+    <hr>
+    <a href="/"><h3 style="text-align: center" >返回主界面</h3><a>
+    <img style="width: 98vw" src="https://s3.bmp.ovh/imgs/2021/09/21ee78aeae84b207.jpg">'''
+    return HttpResponse(line)
+
