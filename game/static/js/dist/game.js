682729db69d46b934fdad5ecc96afc3d03e4ca5e
shi guang <shiguang124@outlook.com>
2021-11-04 23:01:53 +0800

    half

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -6,9 +6,29 @@ class ShiGameMenu
         this.root = root;
         this.$menu = $(`
 <div class="shi_game_menu">
+    <div class="shi_game_menu">
+        <div class="shi_game_menu_filed">
+            <div class="shi_game_menu_filed_item shi_game_menu_filed_item_single">
+                    单人模式
+                </div>
+                <br>
+                <div class="shi_game_menu_filed_item shi_game_menu_filed_item_multi">
+                    多人模式
+                </div>
+                <br>
+                <div class="shi_game_menu_filed_item shi_game_menu_filed_item_settings">
+                    设置
+                </div>
+        </div>
+    </div>
 </div>
 `);
         this.root.$shi_game.append(this.$menu);
+        this.$single = this.$menu.find('.shi_game_menu_filed_item_single');
+        this.$multi = this.$menu.find('.shi_game_menu_filed_item_multi');
+        this.$settings = this.$menu.find('.shi_game_menu_filed_item_settings');
+        
+
     }
 }class ShiGame 
 {
